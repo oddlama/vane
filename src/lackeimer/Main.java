82f8445e2de09fcb38b42b124faebb10ec5fe303
oddlama <oddlama@oddlama.org>
2020-08-07 00:32:10 +0200

    Initial commit, added clang format, pom, and some setup

---
+++ src/lackeimer/Main.java
@@ -0,0 +1,36 @@
+package lackeimer;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	@Override
+	public void onLoad() {
+	}
+
+	@Override
+	public void onEnable() {
+		// Connect to database
+
+		// Enable dynmap integration
+
+		// Register listeners
+
+		// Register commands
+
+		// Schedule shutdown if there are no players
+		if (getServer().getOnlinePlayers().isEmpty()) {
+		}
+	}
+
+	@Override
+	public void onDisable() {
+		// Unregister commands
+
+		// Unregister listeners
+
+		// Disable dynmap integration
+
+		// Close database
+	}
+}
