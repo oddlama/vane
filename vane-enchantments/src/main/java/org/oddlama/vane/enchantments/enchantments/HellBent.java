2c11799fac28c21549180301c69764d5ed0901d4
oddlama <oddlama@oddlama.org>
2020-08-28 19:37:23 +0200

    Added HellBent enchantment

--- vane-enchantments/src/main/java/org/oddlama/vane/enchantments/enchantments/HellBent.java
+++ vane-enchantments/src/main/java/org/oddlama/vane/enchantments/enchantments/HellBent.java
@@ -6,10 +6,36 @@ import org.oddlama.vane.core.module.Context;
 import org.oddlama.vane.enchantments.CustomEnchantment;
 import org.oddlama.vane.enchantments.Enchantments;
 import org.oddlama.vane.annotation.enchantment.Rarity;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 
 @VaneEnchantment(name = "hell_bent", rarity = Rarity.COMMON, treasure = true, target = EnchantmentTarget.ARMOR_HEAD)
 public class HellBent extends CustomEnchantment<Enchantments> {
 	public HellBent(Context<Enchantments> context) {
 		super(context);
+	}
+
+	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+	public void on_player_damage(final EntityDamageEvent event) {
+		final var entity = event.getEntity();
+		if (!(entity instanceof Player) || event.getCause() != EntityDamageEvent.DamageCause.FLY_INTO_WALL) {
+			return;
+		}
+
+		// Get helmet
+		final var player = (Player)entity;
+		final var helmet = player.getEquipment().getHelmet();
+		if (helmet == null) {
+			return;
+		}
+
+		// Check enchantment
+		if (helmet.getEnchantmentLevel(this.bukkit()) == 0) {
+			return;
+		}
+
+		event.setCancelled(true);
 	}
 }
