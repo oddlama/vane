291869c276197d3e9929a372126126dd12d3afa0
oddlama <oddlama@oddlama.org>
2020-10-02 21:51:04 +0200

    Remove debug prints, allow >16 characters on spectator display name

--- vane-core/src/main/java/org/oddlama/vane/core/Core.java
+++ vane-core/src/main/java/org/oddlama/vane/core/Core.java
@@ -280,19 +280,13 @@ public class Core extends Module<Core> implements PluginMessageListener {
 	public synchronized String auth_multiplex_player_name(final UUID uuid) {
 		final var original_player_id = storage_auth_multiplex.get(uuid);
 		final var multiplexer_id = storage_auth_multiplexer_id.get(uuid);
-		System.out.println("auth_multiplex_player_name " + uuid + " → " + original_player_id + " → " + multiplexer_id);
 		if (original_player_id == null || multiplexer_id == null) {
 			return null;
 		}
 
 		final var original_player = getServer().getOfflinePlayer(original_player_id);
-		System.out.println("offl " + original_player);
 		if (original_player != null) {
-			var str = "§7[" + multiplexer_id + "]§r " + original_player.getName();
-			if (str.length() > 16) {
-				str = str.substring(0, 16);
-			}
-			return str;
+			return "§7[" + multiplexer_id + "]§r " + original_player.getName();
 		}
 
 		return null;
@@ -301,9 +295,15 @@ public class Core extends Module<Core> implements PluginMessageListener {
 	private void try_init_multiplexed_player_name(final Player player) {
 		final var id = player.getUniqueId();
 		final var display_name = auth_multiplex_player_name(id);
-		System.out.println("try_init_multiplexed_player_name " + player + " " + id + " dpn " + display_name);
 		if (display_name == null) {
 			return;
+		}
+
+		final String stripped_name;
+		if (display_name.length() > 16) {
+			stripped_name = display_name.substring(0, 16);
+		} else {
+			stripped_name = display_name;
 		}
 
 		log.info("[multiplex] Init player '" + display_name + "' for registered auth multiplexed player {" + id + ", " + player.getName() + "}");
