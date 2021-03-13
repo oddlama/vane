e31e4f4846e553f0c64fc7ac625a6cccb6551178
oddlama <oddlama@oddlama.org>
2021-03-13 18:30:19 +0100

    Add region intersection tests

--- vane-regions/src/main/java/org/oddlama/vane/regions/region/RegionExtent.java
+++ vane-regions/src/main/java/org/oddlama/vane/regions/region/RegionExtent.java
@@ -112,7 +112,46 @@ public class RegionExtent {
 			return false;
 		}
 
-		// TODO check
+		final var l = min();
+		final var h = max();
+		return block.getX() >= l.getX() && block.getX() <= h.getX()
+		    && block.getY() >= l.getY() && block.getY() <= h.getY()
+		    && block.getZ() >= l.getZ() && block.getZ() <= h.getZ();
+	}
+
+	public boolean intersects_extent(final RegionExtent other) {
+		if (!min().getWorld().equals(other.min().getWorld())) {
+			return false;
+		}
+
+		final var l1 = min();
+		final var h1 = max();
+		final var l2 = other.min();
+		final var h2 = other.max();
+
+		// Compute global min and max for each axis
+		final var llx = Math.min(l1.getX(), l2.getX());
+		final var lly = Math.min(l1.getY(), l2.getY());
+		final var llz = Math.min(l1.getZ(), l2.getZ());
+		final var hhx = Math.max(h1.getX(), h2.getX());
+		final var hhy = Math.max(h1.getY(), h2.getY());
+		final var hhz = Math.max(h1.getZ(), h2.getZ());
+
+		// Compute global extent length
+		final var extent_global_x = (hhx - llx) + 1;
+		final var extent_global_y = (hhy - lly) + 1;
+		final var extent_global_z = (hhz - llz) + 1;
+
+		// Compute sum of local extent lengths
+		final var extent_sum_x = (h2.getX() - l2.getX()) + (h1.getX() - l1.getX()) + 2;
+		final var extent_sum_y = (h2.getY() - l2.getY()) + (h1.getY() - l1.getY()) + 2;
+		final var extent_sum_z = (h2.getZ() - l2.getZ()) + (h1.getZ() - l1.getZ()) + 2;
+
+		// It intersects exactly when:
+		//   for all a in axis: global_extent(a) < individual_extent_sum(a)
+		return extent_global_x < extent_sum_x
+			&& extent_global_y < extent_sum_y
+			&& extent_global_z < extent_sum_z;
 	}
 
 	public boolean intersects_chunk(final Chunk chunk) {
@@ -120,6 +159,30 @@ public class RegionExtent {
 			return false;
 		}
 
-		// TODO return true iff any block inside extent is inside chunk.
+		final var l1 = min();
+		final var h1 = max();
+		final var l2x = chunk.getX() * 16;
+		final var l2z = chunk.getZ() * 16;
+		final var h2x = (chunk.getX() + 1) * 16 - 1;
+		final var h2z = (chunk.getZ() + 1) * 16 - 1;
+
+		// Compute global min and max for each axis
+		final var llx = Math.min(l1.getX(), l2x);
+		final var llz = Math.min(l1.getZ(), l2z);
+		final var hhx = Math.max(h1.getX(), h2x);
+		final var hhz = Math.max(h1.getZ(), h2z);
+
+		// Compute global extent length
+		final var extent_global_x = (hhx - llx) + 1;
+		final var extent_global_z = (hhz - llz) + 1;
+
+		// Compute sum of local extent lengths
+		final var extent_sum_x = (h2x - l2x) + (h1.getX() - l1.getX()) + 2;
+		final var extent_sum_z = (h2z - l2z) + (h1.getZ() - l1.getZ()) + 2;
+
+		// It intersects exactly when:
+		//   for all a in axis: global_extent(a) < individual_extent_sum(a)
+		return extent_global_x < extent_sum_x
+			&& extent_global_z < extent_sum_z;
 	}
 }
