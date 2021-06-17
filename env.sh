80713ee29a721483e6e0280ea2221f563e0b3464
oddlama <oddlama@oddlama.org>
2021-06-17 15:51:24 +0200

    Begin 1.17 conversion

--- env.sh
+++ env.sh
@@ -1,3 +1,3 @@
-export PATH="/usr/lib64/openjdk-11/bin:$PATH"
-export JAVA_HOME="/usr/lib64/openjdk-11"
+export PATH="/home/malte/openjdk-16/bin:$PATH"
+export JAVA_HOME="/home/malte/openjdk-16"
 export JDK_HOME="$(realpath "$JAVA_HOME")/"
