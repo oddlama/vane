d7be62e882967b37db19ca1e408b253f497ff6e1
oddlama <oddlama@oddlama.org>
2021-12-13 15:08:06 +0100

    feat: allow configuration of fast-walking speed effect level (closes #74)

--- vane-trifles/src/main/java/org/oddlama/vane/trifles/FastWalkingGroup.java
+++ vane-trifles/src/main/java/org/oddlama/vane/trifles/FastWalkingGroup.java
@@ -6,12 +6,16 @@ import java.util.Set;
 import org.bukkit.Material;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.oddlama.vane.annotation.config.ConfigInt;
 import org.oddlama.vane.annotation.config.ConfigLong;
 import org.oddlama.vane.annotation.config.ConfigMaterialSet;
 import org.oddlama.vane.core.module.Context;
 import org.oddlama.vane.core.module.ModuleGroup;
 
 public class FastWalkingGroup extends ModuleGroup<Trifles> {
+
+	@ConfigInt(def = 2, min = 1, max = 10, desc = "Speed effect level.")
+	public int config_speed_level;
 
 	@ConfigLong(def = 2000, min = 50, max = 5000, desc = "Speed effect duration in milliseconds.")
 	public long config_duration;
@@ -30,7 +34,7 @@ public class FastWalkingGroup extends ModuleGroup<Trifles> {
 	public void on_config_change() {
 		var ticks = ms_to_ticks(config_duration);
 		walk_speed_effect =
-			new PotionEffect(PotionEffectType.SPEED, (int) ticks, 1)
+			new PotionEffect(PotionEffectType.SPEED, (int) ticks, config_speed_level - 1)
 				.withAmbient(false)
 				.withParticles(false)
 				.withIcon(false);
