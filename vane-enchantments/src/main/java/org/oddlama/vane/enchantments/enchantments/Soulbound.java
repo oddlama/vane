62cbd52ee3b500e82546119cf22c2ffd638c374f
ryantheleach <rleach@rleach.id.au>
2022-01-10 10:39:18 +1030

    feature: Soulbound items can now be dropped without a chest by double dropping from inventory within a cooldown period.

--- vane-enchantments/src/main/java/org/oddlama/vane/enchantments/enchantments/Soulbound.java
+++ vane-enchantments/src/main/java/org/oddlama/vane/enchantments/enchantments/Soulbound.java
@@ -1,22 +1,33 @@
 package org.oddlama.vane.enchantments.enchantments;
 
+import static org.oddlama.vane.util.Util.namespaced_key;
+
 import java.util.ArrayList;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.EnchantmentStorageMeta;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.loot.LootTables;
+import org.oddlama.vane.annotation.config.ConfigLong;
 import org.oddlama.vane.annotation.enchantment.Rarity;
 import org.oddlama.vane.annotation.enchantment.VaneEnchantment;
+import org.oddlama.vane.annotation.lang.LangMessage;
 import org.oddlama.vane.core.LootTable.LootTableEntry;
+import org.oddlama.vane.core.data.CooldownData;
 import org.oddlama.vane.core.item.CustomItem;
+import org.oddlama.vane.core.lang.TranslatedMessage;
 import org.oddlama.vane.core.module.Context;
 import org.oddlama.vane.enchantments.CustomEnchantment;
 import org.oddlama.vane.enchantments.Enchantments;
@@ -26,8 +37,36 @@ import org.oddlama.vane.enchantments.items.BookVariant;
 @VaneEnchantment(name = "soulbound", rarity = Rarity.RARE, treasure = true, allow_custom = true)
 public class Soulbound extends CustomEnchantment<Enchantments> {
 
+	@ConfigLong(
+		def = 2000,
+		min = 0,
+		desc = "Window to allow Soulbound item drop immediately after a previous drop in milliseconds"
+	)
+	public long config_cooldown;
+
+	private static final NamespacedKey IGNORE_SOULBOUND_DROP = namespaced_key(
+		"vane_enchantments",
+		"ignore_soulbound_drop"
+	);
+	private CooldownData drop_cooldown = new CooldownData(IGNORE_SOULBOUND_DROP, config_cooldown);
+
+	@LangMessage
+	public TranslatedMessage lang_drop_lock_warning;
+
+	@LangMessage
+	public TranslatedMessage lang_dropped_notification;
+
+	@LangMessage
+	public TranslatedMessage lang_drop_cooldown;
+
 	public Soulbound(Context<Enchantments> context) {
 		super(context);
+	}
+
+	@Override
+	public void on_config_change() {
+		super.on_config_change();
+		drop_cooldown = new CooldownData(IGNORE_SOULBOUND_DROP, config_cooldown);
 	}
 
 	@Override
@@ -105,10 +144,32 @@ public class Soulbound extends CustomEnchantment<Enchantments> {
 		final var it = event.getDrops().iterator();
 		while (it.hasNext()) {
 			final var drop = it.next();
-			if (drop.getEnchantmentLevel(this.bukkit()) > 0) {
+			if (is_soulbound(drop)) {
 				keep_items.add(drop);
 				it.remove();
 			}
+		}
+	}
+
+	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+	public void on_player_inventory_check(final InventoryClickEvent event) {
+		if (event.getCursor() == null) return;
+		if (!is_soulbound(event.getCursor())) return;
+		if (
+			event.getAction() == InventoryAction.DROP_ALL_CURSOR || event.getAction() == InventoryAction.DROP_ONE_CURSOR
+		) {
+			boolean too_slow = drop_cooldown.peek_cooldown(event.getCursor().getItemMeta());
+			if (too_slow) {
+				// Dropped too slow, refresh and cancel
+				final ItemMeta meta = event.getCursor().getItemMeta();
+				drop_cooldown.check_or_update_cooldown(meta);
+				event.getCursor().setItemMeta(meta);
+				lang_drop_cooldown.send_action_bar(event.getWhoClicked());
+				event.setResult(Event.Result.DENY);
+				return;
+			}
+			return;
+			// else allow as normal
 		}
 	}
 
@@ -118,11 +179,20 @@ public class Soulbound extends CustomEnchantment<Enchantments> {
 		// Prevents yeeting your best sword out of existence.
 		// (It's okay to put them into chests)
 		final var dropped_item = event.getItemDrop().getItemStack();
-		if (dropped_item.getEnchantmentLevel(this.bukkit()) > 0) {
+		if (is_soulbound(dropped_item)) {
+			boolean too_slow = drop_cooldown.peek_cooldown(dropped_item.getItemMeta());
+			if (!too_slow) {
+				var meta = dropped_item.getItemMeta();
+				drop_cooldown.clear(dropped_item.getItemMeta());
+				dropped_item.setItemMeta(meta);
+				lang_dropped_notification.send(event.getPlayer(), dropped_item.displayName());
+				return;
+			}
 			final var inventory = event.getPlayer().getInventory();
 			if (inventory.firstEmpty() != -1) {
 				// We still have space in the inventory, so the player tried to drop it with Q.
 				event.setCancelled(true);
+				lang_drop_lock_warning.send_action_bar(event.getPlayer(), event.getItemDrop().getItemStack().displayName());
 			} else {
 				// Inventory is full (e.g. when exiting crafting table with soulbound item in it)
 				// so we drop the first non-soulbound item (if any) instead.
@@ -149,8 +219,13 @@ public class Soulbound extends CustomEnchantment<Enchantments> {
 				final var player = event.getPlayer();
 				inventory.setItem(non_soulbound_item_slot, dropped_item);
 				player.getLocation().getWorld().dropItem(player.getLocation(), non_soulbound_item);
+				lang_drop_lock_warning.send_action_bar(player, event.getItemDrop().getItemStack().displayName());
 				event.setCancelled(true);
 			}
 		}
+	}
+
+	private boolean is_soulbound(ItemStack dropped_item) {
+		return dropped_item.getEnchantmentLevel(this.bukkit()) > 0;
 	}
 }
