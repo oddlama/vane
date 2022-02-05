6a1a7c7e3aed677fa25ae18d2b2db2203070e7a4
oddlama <oddlama@oddlama.org>
2022-02-05 04:07:51 +0100

    feat: all entites can now be portalled. closes #37

--- vane-portals/src/main/java/org/oddlama/vane/portals/PortalTeleporter.java
+++ vane-portals/src/main/java/org/oddlama/vane/portals/PortalTeleporter.java
@@ -6,12 +6,14 @@ import java.util.HashMap;
 import java.util.UUID;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.EntityTeleportEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerPortalEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 import org.oddlama.vane.core.Listener;
+import org.oddlama.vane.core.event.EntityMoveEvent;
 import org.oddlama.vane.core.module.Context;
 
 public class PortalTeleporter extends Listener<Portals> {
@@ -68,12 +70,12 @@ public class PortalTeleporter extends Listener<Portals> {
 	}
 
 	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
-	public void on_player_move(final PlayerMoveEvent event) {
-		final var player = event.getPlayer();
-		final var player_id = player.getUniqueId();
+	public void on_entity_move(final EntityMoveEvent event) {
+		final var entity = event.getEntity();
+		final var entity_id = entity.getUniqueId();
 		final var block = event.getTo().getBlock();
 
-		if (!entities_portalling.containsKey(player_id)) {
+		if (!entities_portalling.containsKey(entity_id)) {
 			// Check if we walked into a portal
 			if (!get_module().portal_area_materials.contains(block.getType())) {
 				return;
@@ -90,21 +92,21 @@ public class PortalTeleporter extends Listener<Portals> {
 			}
 
 			// Put null to signal initiated teleportation
-			entities_portalling.put(player_id, null);
+			entities_portalling.put(entity_id, null);
 
 			var target_location = target.spawn().clone();
-			// Increase Y value if player is currently flying through a portal that
+			// Increase Y value if entity is currently flying through a portal that
 			// has extent in the y direction (i.e. is built upright)
-			if (player.isGliding() && portal.orientation().plane().y()) {
+			if (entity instanceof LivingEntity living_entity && living_entity.isGliding() && portal.orientation().plane().y()) {
 				target_location.setY(target_location.getY() + 1.5);
 			}
 
-			final var player_location = player.getLocation();
-			target_location.setPitch(player_location.getPitch());
-			target_location.setYaw(player_location.getYaw());
+			final var entity_location = entity.getLocation();
+			target_location.setPitch(entity_location.getPitch());
+			target_location.setYaw(entity_location.getYaw());
 
 			// If the exit orientation of the target portal is locked, we make sure that
-			// the orientation of the entered portal is flipped, if a player enters from the back.
+			// the orientation of the entered portal is flipped, if an entity (player) enters from the back.
 			// We have to flip the source portal orientation, if the vector to be transformed
 			// is NOT opposing the source portal vector (i.e. not pointing against the front).
 			// Calculate new location (pitch, yaw) and velocity.
@@ -112,27 +114,26 @@ public class PortalTeleporter extends Listener<Portals> {
 				portal.orientation().apply(target.orientation(), target_location, target.exit_orientation_locked());
 			final var new_velocity = portal
 				.orientation()
-				.apply(target.orientation(), player.getVelocity(), target.exit_orientation_locked());
+				.apply(target.orientation(), entity.getVelocity(), target.exit_orientation_locked());
 
 			// Set new movement location
-			event.setTo(target_location);
+			entity.teleport(target_location, PlayerTeleportEvent.TeleportCause.PLUGIN);
 
 			// Retain velocity, but do so next tick because apparently
 			// the movement event overrides the velocity.
-			// FIXME:ideally this would be done the same tick as the movement.
 			schedule_next_tick(() -> {
-				player.setVelocity(new_velocity);
+				entity.setVelocity(new_velocity);
 			});
 		} else {
-			final var loc = entities_portalling.get(player_id);
+			final var loc = entities_portalling.get(entity_id);
 			if (loc == null) {
 				// Initial teleport. Remember current location, so we can check
 				// that the entity moved away far enough to allow another teleport
-				entities_portalling.put(player_id, event.getFrom().clone());
+				entities_portalling.put(entity_id, event.getFrom().clone());
 			} else if (!get_module().portal_area_materials.contains(block.getType())) {
 				// At least 2 blocks away and outside of portal area → finish portalling.
 				if (event.getFrom().distance(loc) > 2.0) {
-					entities_portalling.remove(player_id);
+					entities_portalling.remove(entity_id);
 				}
 			}
 		}
