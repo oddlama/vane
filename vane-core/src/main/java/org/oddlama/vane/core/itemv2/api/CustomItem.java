b0a40020cac30636c25c47cb41c54e4db964f6a8
oddlama <oddlama@oddlama.org>
2022-02-09 04:57:39 +0100

    chore(customitems): extract default functionality into helper class

--- vane-core/src/main/java/org/oddlama/vane/core/itemv2/api/CustomItem.java
+++ vane-core/src/main/java/org/oddlama/vane/core/itemv2/api/CustomItem.java
@@ -16,15 +16,6 @@ import net.kyori.adventure.text.TranslatableComponent;
  */
 public interface CustomItem {
 	/**
-	 * Used in persistent item storage to identify custom items.
-	 */
-	public static final NamespacedKey CUSTOM_ITEM_IDENTIFIER = Util.namespaced_key("vane_api", "custom_item_identifier");
-	/**
-	 * Used in persistent item storage to store custom item version.
-	 */
-	public static final NamespacedKey CUSTOM_ITEM_VERSION = Util.namespaced_key("vane_api", "custom_item_version");
-
-	/**
 	 * The unique identifier for this custom item. This is the only value that can never be changed after initial item registration.
 	 * If you want to deprecate an item, you can force your subclass to always return false in {@link #enabled()}. This will cause existing
 	 * items to behave as their base items. To completely remove a custom item from encountered inventories, you can
@@ -57,6 +48,13 @@ public interface CustomItem {
 	 * use a material with less net-worth than what the custom item provides.
 	 * For crafting ingredients, we recommend using an item that has no other use than in crafting.
 	 * Generally it is a good idea to pick materials that have an item with similar properties.
+	 *
+	 * No attempts will be made to remove the vanilla behavior of the base items.
+	 * If you require for example a hoe that doesn't till blocks or a carrot
+	 * on a stick that doesn't attract pigs, you generally will have to listen
+	 * for the corresponding event yourself and cancel it. For some commonly
+	 * used base items, vane provides helpers that disable the vanilla functionality
+	 * for you (see TODO).
 	 */
 	// TODO: intercept brewing stand events. hoe events, shovel tilling etc. Is this too much? We will certainly miss certain things.
 	// TODO: maybe recommend just using the most mundane, fitting item (no durability -> paper, durability -> warped fungus on a stick), now that we have custom durability, this might be the best way.
@@ -90,14 +88,19 @@ public interface CustomItem {
 
 	/**
 	 * A custom translation key that will be used to display durability on the item.
-	 * Return null to disable durability lore.
+	 * Return null to disable durability lore. Arguments to the translatable component
+	 * that will be supplied are the current durability (%1$s) and max durability (%2$s).
 	 */
 	public @Nullable TranslatableComponent durabilityLore();
 
 	/**
-	 * The items effective maximum durability, only effective if {@link #usesCustomDurability()} returns true.
-	 * If this is changed while items of this type already exist with a different maximum durability,
-	 * the items will be updated and keep their current durability, but clamped to the new maximum.
+	 * The items effective maximum durability. If this returns 0, no changes will be made to the base
+	 * item's durability mechanic. If this is set to a value > 0, it requires a base item with durability.
+	 * The durability bar of the base item then acts solely as an indicative value of a separately stored durability.
+	 * Changes to the item's durability by classical means are automatically reflected in this property.
+	 *
+	 * If this is value changed while item stacks of this custom item already exist with a different maximum durability,
+	 * the affected items will be updated and keep their current durability, but clamped to the new maximum.
 	 */
 	public int durability();
 
@@ -119,41 +122,22 @@ public interface CustomItem {
 	 * no base information actually changed, but an item still is considered to
 	 * be updated, for example anvil results.
 	 */
-	default public ItemStack updateItemStack(@NotNull final ItemStack itemStack) {
+	default public ItemStack onUpdateItemStack(@NotNull final ItemStack itemStack) {
 		return itemStack;
-	}
-
-	/**
-	 * Internal function. Used as a dispatcher to update internal information and then call
-	 * {@link #updateItemStack(ItemStack)} to let the user update information. This prevents
-	 * problems with information de-sync in case the user would forget to call super.
-	 */
-	default public ItemStack _updateItemStack(@NotNull final ItemStack itemStack) {
-		itemStack.editMeta(meta -> {
-			final var data = meta.getPersistentDataContainer();
-			data.set(CUSTOM_ITEM_IDENTIFIER, PersistentDataType.STRING, key().toString());
-			data.set(CUSTOM_ITEM_VERSION, PersistentDataType.INTEGER, version());
-			meta.setCustomModelData(customModelData());
-		});
-		return updateItemStack(itemStack);
 	}
 
 	/**
 	 * Creates a new item stack with a single item of this custom item.
 	 */
 	default public ItemStack newStack() {
-		return newStack(1);
+		return CustomItemHelper.newStack(this);
 	}
 
 	/**
 	 * Creates a new item stack with the given amount of items of this custom item.
 	 */
 	default public ItemStack newStack(final int amount) {
-		final var itemStack = new ItemStack(baseMaterial(), amount);
-		itemStack.editMeta(meta -> {
-			meta.displayName(displayName());
-		});
-		return _updateItemStack(itemStack);
+		return CustomItemHelper.newStack(this, amount);
 	}
 
 	/**
@@ -163,8 +147,6 @@ public interface CustomItem {
 	 * when those two items are different CustomItem definitions but otherwise share attributes and functionality.
 	 */
 	default public ItemStack convertExistingStack(ItemStack itemStack) {
-		itemStack = itemStack.clone();
-		itemStack.setType(baseMaterial());
-		return _updateItemStack(itemStack);
+		return CustomItemHelper.convertExistingStack(this, itemStack);
 	}
 }
