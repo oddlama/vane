6371ff6c5a6746be71dab7277ad4df0d4db5f804
oddlama <oddlama@oddlama.org>
2022-06-24 15:40:12 +0200

    wip: more overview html tailwindcss prototyping

---
+++ docs/overview/tw/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+module.exports = {
+	content: ["../dist/index.html"],
+	theme: {
+		extend: {},
+	},
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('flowbite/plugin')
+	],
+}
