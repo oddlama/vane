6f1fac3ee1f47e5a508dda56a6341203c5ef9ee1
oddlama <oddlama@oddlama.org>
2022-06-29 15:25:50 +0200

    fix: hotfix - bump language file version

--- vane-enchantments/src/main/java/org/oddlama/vane/enchantments/Enchantments.java
+++ vane-enchantments/src/main/java/org/oddlama/vane/enchantments/Enchantments.java
@@ -3,7 +3,7 @@ package org.oddlama.vane.enchantments;
 import org.oddlama.vane.annotation.VaneModule;
 import org.oddlama.vane.core.module.Module;
 
-@VaneModule(name = "enchantments", bstats = 8640, config_version = 1, lang_version = 3, storage_version = 1)
+@VaneModule(name = "enchantments", bstats = 8640, config_version = 1, lang_version = 4, storage_version = 1)
 public class Enchantments extends Module<Enchantments> {
 	public Enchantments() {
 		new org.oddlama.vane.enchantments.items.Tomes(this);
