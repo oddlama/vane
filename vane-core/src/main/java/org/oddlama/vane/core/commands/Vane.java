e19c04caf44c08778d99f60b3fde212d808b81d8
oddlama <oddlama@oddlama.org>
2022-08-23 00:23:45 +0200

    feat: add /vane test command to run some basic tests (after major updates)

--- vane-core/src/main/java/org/oddlama/vane/core/commands/Vane.java
+++ vane-core/src/main/java/org/oddlama/vane/core/commands/Vane.java
@@ -1,7 +1,11 @@
 package org.oddlama.vane.core.commands;
 
+import java.util.Random;
+
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.loot.LootContext;
+import org.bukkit.loot.LootTables;
 import org.oddlama.vane.annotation.command.Name;
 import org.oddlama.vane.annotation.lang.LangMessage;
 import org.oddlama.vane.core.Core;
@@ -36,6 +40,7 @@ public class Vane extends Command<Core> {
 		reload.choose_module().exec(this::reload_module);
 
 		params().fixed("generate_resource_pack").ignore_case().exec(this::generate_resource_pack);
+		params().fixed("test_do_not_use_if_you_are_not_a_dev").ignore_case().exec(this::test);
 	}
 
 	private void reload_module(CommandSender sender, Module<?> module) {
@@ -63,6 +68,34 @@ public class Vane extends Command<Core> {
 			var dist = get_module().resource_pack_distributor;
 			dist.update_sha1(file);
 			dist.send_resource_pack((Player) sender);
+		}
+	}
+
+	private void test(CommandSender sender) {
+		final var loot_table = LootTables.ABANDONED_MINESHAFT.getLootTable();
+		Random r = new Random();
+		int tomes = 0;
+		final var simulation_count = 10000;
+		get_module().log.info("Testing ancient tome generation...");
+
+		for (int i = 0; i < simulation_count; ++i) {
+			final var loot = loot_table.populateLoot(r, (new LootContext.Builder(get_module().getServer().getWorlds().get(0).getBlockAt(0,0,0).getLocation())).build());
+			for (final var is : loot) {
+				if (is.hasItemMeta()) {
+					final var meta = is.getItemMeta();
+					if (meta.hasCustomModelData() && is.getItemMeta().getCustomModelData() == 0x770000) {
+						++tomes;
+					}
+				}
+			}
+		}
+
+		if (tomes == 0) {
+			get_module().log.severe("0 tomes were generated in " + simulation_count + " chests.");
+		} else if (tomes > 0.25 * simulation_count * 0.7 && tomes < 0.25 * simulation_count / 0.7) { // 70% tolerance to lower bound
+			get_module().log.warning(tomes + " tomes were generated in " + simulation_count + " chests. This is " + (100.0 * (tomes / simulation_count) / 0.25) + "% of the expected value.");
+		} else {
+			get_module().log.info(tomes + " tomes were generated in " + simulation_count + " chests. This is " + (100.0 * (tomes / simulation_count) / 0.25) + "% of the expected value.");
 		}
 	}
 }
