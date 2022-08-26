12fd92b611d691c8369dc703f2dd0eeef4f9ec27
oddlama <oddlama@oddlama.org>
2022-08-26 02:37:00 +0200

    feat: implement True North Compass (closes #115)

--- vane-trifles/src/main/java/org/oddlama/vane/trifles/Trifles.java
+++ vane-trifles/src/main/java/org/oddlama/vane/trifles/Trifles.java
@@ -32,5 +32,6 @@ public class Trifles extends Module<Trifles> {
 		new org.oddlama.vane.trifles.items.EmptyXpBottle(this);
 		xp_bottles = new org.oddlama.vane.trifles.items.XpBottles(this);
 		new org.oddlama.vane.trifles.items.Trowel(this);
+		new org.oddlama.vane.trifles.items.NorthCompass(this);
 	}
 }
