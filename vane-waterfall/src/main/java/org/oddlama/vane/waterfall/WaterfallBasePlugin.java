ce09b1793ef6afda6385bb3630124f74afcd97ab
Serial <69764315+Serial-ATA@users.noreply.github.com>
2022-08-29 10:02:49 -0400

    chore: add WaterfallBasePlugin

---
+++ vane-waterfall/src/main/java/org/oddlama/vane/waterfall/WaterfallBasePlugin.java
@@ -0,0 +1,19 @@
+package org.oddlama.vane.waterfall;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class WaterfallBasePlugin extends Plugin {
+	Waterfall waterfall;
+
+	@Override
+	public void onEnable() {
+		waterfall = new Waterfall(this);
+		waterfall.enable();
+	}
+
+	@Override
+	public void onDisable() {
+		waterfall.disable();
+	}
+
+}
