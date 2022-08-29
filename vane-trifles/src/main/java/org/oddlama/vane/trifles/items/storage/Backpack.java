517049ed87c3106d5ce28d4d6da49fef5c9b0dc4
oddlama <oddlama@oddlama.org>
2022-08-29 23:15:33 +0200

    chore: finalize backpack

--- vane-trifles/src/main/java/org/oddlama/vane/trifles/items/storage/Backpack.java
+++ vane-trifles/src/main/java/org/oddlama/vane/trifles/items/storage/Backpack.java
@@ -2,29 +2,25 @@ package org.oddlama.vane.trifles.items.storage;
 
 import static org.oddlama.vane.util.PlayerUtil.swing_arm;
 
-import java.util.ArrayList;
 import java.util.EnumSet;
 
-import org.bukkit.DyeColor;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
 import org.bukkit.Sound;
+import org.bukkit.Tag;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.oddlama.vane.annotation.item.VaneItem;
-import org.oddlama.vane.core.config.recipes.RecipeDefinition;
 import org.oddlama.vane.core.config.recipes.RecipeList;
 import org.oddlama.vane.core.config.recipes.SmithingRecipeDefinition;
 import org.oddlama.vane.core.item.CustomItem;
 import org.oddlama.vane.core.item.api.InhibitBehavior;
 import org.oddlama.vane.core.module.Context;
 import org.oddlama.vane.trifles.Trifles;
-import org.oddlama.vane.util.MaterialUtil;
 
-@VaneItem(name = "backpack", base = Material.SHULKER_BOX, model_data = 0x760017 /* until 0x760027, inclusive */, version = 1)
+@VaneItem(name = "backpack", base = Material.SHULKER_BOX, model_data = 0x760017, version = 1)
 public class Backpack extends CustomItem<Trifles> {
 	public Backpack(Context<Trifles> context) {
 		super(context);
@@ -32,26 +28,11 @@ public class Backpack extends CustomItem<Trifles> {
 
 	@Override
 	public RecipeList default_recipes() {
-		final var list = new ArrayList<RecipeDefinition>();
-		list.add(new SmithingRecipeDefinition("from_shulker_box")
-			.base(Material.SHULKER_BOX)
+		return RecipeList.of(new SmithingRecipeDefinition("from_shulker_box")
+			.base(Tag.SHULKER_BOXES)
 			.addition(Material.NETHERITE_INGOT)
 			.copy_nbt(true)
 			.result(key().toString()));
-
-		for (final var color : DyeColor.values()) {
-			final var color_name = color.toString().toLowerCase();
-			list.add(new SmithingRecipeDefinition("from_" + color_name + "_shulker_box")
-				.base(MaterialUtil.material_from(NamespacedKey.minecraft(color_name + "_shulker_box")))
-				.addition(Material.NETHERITE_INGOT)
-				.copy_nbt(true)
-				// TODO custom model data
-				//.result(key().toString() + "[]"));
-				.result(key().toString()));
-			// TODO remove color how?
-		}
-
-		return new RecipeList(list);
 	}
 
 	// ignoreCancelled = false to catch right-click-air events
