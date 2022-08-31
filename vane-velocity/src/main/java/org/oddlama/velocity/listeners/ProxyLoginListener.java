1b1199a36ba66dc10a539a2009033e2b98250c36
Serial <69764315+Serial-ATA@users.noreply.github.com>
2022-08-31 01:34:16 -0400

    chore: add ProxyPingListener

--- vane-velocity/src/main/java/org/oddlama/velocity/listeners/ProxyLoginListener.java
+++ vane-velocity/src/main/java/org/oddlama/velocity/listeners/ProxyLoginListener.java
@@ -4,15 +4,13 @@ import com.google.inject.Inject;
 import com.velocitypowered.api.event.PostOrder;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.proxy.ProxyServer;
-import com.velocitypowered.api.proxy.server.RegisteredServer;
 import org.oddlama.vane.proxycore.listeners.LoginEvent;
 import org.oddlama.velocity.Velocity;
 import org.oddlama.velocity.compat.VelocityCompatServerInfo;
 import org.oddlama.velocity.compat.event.VelocityCompatLoginEvent;
 import org.oddlama.velocity.compat.event.VelocityCompatPendingConnection;
 
-import java.util.List;
-import java.util.Map;
+import static org.oddlama.velocity.Util.get_server_for_host;
 
 public class ProxyLoginListener {
 
@@ -32,17 +30,7 @@ public class ProxyLoginListener {
 		final var virtual_host = event.getPlayer().getVirtualHost();
 		if (virtual_host.isEmpty()) return;
 
-		Map<String, List<String>> forced_hosts = proxy.getConfiguration().getForcedHosts();
-
-		String forced;
-		RegisteredServer server;
-		try {
-			forced = forced_hosts.get(virtual_host.get().getHostString()).get(0);
-			if (forced == null || forced.isEmpty()) throw new Exception();
-			server = proxy.getServer(forced).get();
-		} catch (Exception ignored) {
-			server = proxy.getServer(proxy.getConfiguration().getAttemptConnectionOrder().get(0)).get();
-		}
+		final var server = get_server_for_host(proxy, virtual_host.get());
 
 		var server_info = new VelocityCompatServerInfo(server);
 		LoginEvent proxy_event = new VelocityCompatLoginEvent(event, velocity, server_info, new VelocityCompatPendingConnection(event.getPlayer()));
