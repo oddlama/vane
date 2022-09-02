e460ea7ce521b4964481259bcea8906adf16c5a5
Serial <69764315+Serial-ATA@users.noreply.github.com>
2022-09-02 12:14:59 -0400

    chore: move default config to `proxy-core` resources

--- vane-proxy-core/src/main/java/org/oddlama/vane/proxycore/config/ConfigManager.java
+++ vane-proxy-core/src/main/java/org/oddlama/vane/proxycore/config/ConfigManager.java
@@ -4,8 +4,8 @@ import org.jetbrains.annotations.Nullable;
 import org.oddlama.vane.proxycore.VaneProxyPlugin;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.*;
 import java.util.logging.Level;
 
@@ -25,15 +25,17 @@ public class ConfigManager {
 		return new File(plugin.get_data_folder(), "config.toml");
 	}
 
+	@SuppressWarnings("BooleanMethodIsAlwaysInverted")
 	public boolean load() {
 		final var file = file();
-		if (!file.exists()) {
-			save_default(file);
+		if (!file.exists() && !save_default(file)) {
+			plugin.get_logger().log(Level.SEVERE, "Unable to create default config! Bailing.");
+			return false;
 		}
 
 		Config parsed_config;
 		try {
-			parsed_config = new Config(file());
+			parsed_config = new Config(file);
 		} catch (Exception e) {
 			plugin.get_logger().log(Level.SEVERE, "Error while loading config file '" + file + "'", e);
 			return false;
@@ -61,86 +63,19 @@ public class ConfigManager {
 		return true;
 	}
 
-	public void save_default(final File file) {
-		file.getParentFile().mkdirs();
-		final var content = """
-				[auth_multiplex]
-				   \s
-				    # Define your auth multiplexers
-				    # These allow the specified UUIDs to join the server
-				    # multiple times with a fake UUID. IDs must be > 0.
-				   \s
-				    # Example:
-				    #
-				    # [auth_multiplex.ID_NUMBER]
-				    # port = 25566
-				    # allowed_uuids = ["b4a6717f-e3ab-4348-8e18-022827ef3177"]
-				    #
-				    # (Where ID_NUMBER is a nonzero integer)
-				    #
-				    # This will allow *only* UUID "b4a6717f-e3ab-4348-8e18-022827ef3177"
-				    # to use this multiplexer. You can add as many UUIDs as you want.
-				    #
-				    # To make it public, simply leave out the `allowed_uuids` field.
-				    #
-				    # It is *not* possible to have multiple multiplexers on the same port.
-				   \s
-				[managed_servers]
-
-				    # Define your managed servers
-				    #
-				    # NOTE: No field is necessary! You can leave out anything you don't
-				    #       want, including entire sections!
-
-				    # Example:
-				    #
-				    #   [managed_servers.my_server]
-				    #
-				    #   # The server display name (used for messages and substitutions)
-				    #
-				    #   display_name = "§6> §6§lMy Minecraft Server§6 <"
-				    #
-				    #       # The online configuration, things to show in the server list
-				    #       # when the server is online
-				    #
-				    #       [managed_servers.my_server.online]
-				    #
-				    #       # The server favicon (absolute path or relative to proxy directory)
-				    #       favicon = "../%SERVER%/server-icon.png"
-				    #
-				    #       # The motd for the server list (up to two lines)
-				    #       motd = "%SERVER_DISPLAY_NAME%\\n§aowo§7 | %QUOTE%"
-				    #
-				    #       # A list of quotes for substitutions
-				    #       quotes = ["Ah, yes."]
-				    #
-				    #       # The offline configuration, for when the server is offline
-				    #
-				    #       [managed_servers.my_server.offline]
-				    #       favicon = "../%SERVER%/server-icon-offline.png"
-				    #       motd = "%SERVER_DISPLAY_NAME%\\n§cowo§7 | %QUOTE%"
-				    #       quotes = ["Paniik!"]
-				    #
-				    #       # Automatic start settings
-				    #       [managed_servers.my_server.start]
-				    #
-				    #       # The command to execute when the first player joins while the server is offline
-				    #       # %SERVER%: The server identifier
-				    #
-				    #       cmd = ["/path/to/startscript", "arg1", "will also substitute %SERVER%"]
-				    #
-				    #       # The kick message to send to the player that started the server
-				    #       # %SERVER%: The server identifier
-				    #       # %SERVER_DISPLAY_NAME%: The server display name
-				    #
-				    #       kick_msg = "%SERVER_DISPLAY_NAME% §7is being started.\\n§7Try again in §b10 Seconds§7 \\\\(^-^)/"
-				""";
-
-		// Save content to file
+	@SuppressWarnings("ResultOfMethodCallIgnored")
+	public boolean save_default(final File file) {
 		try {
-			Files.writeString(file.toPath(), content);
-		} catch (IOException e) {
+			file.getParentFile().mkdirs();
+			Files.copy(
+					Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("config.toml")),
+					file.toPath(),
+					StandardCopyOption.REPLACE_EXISTING
+			);
+			return true;
+		} catch (Exception e) {
 			plugin.get_logger().log(Level.SEVERE, "Error while writing config file '" + file + "'", e);
+			return false;
 		}
 	}
 
