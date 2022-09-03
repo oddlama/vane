cbda5a9253bd6e6f4155921914e0efa16f3d0b81
Serial <69764315+Serial-ATA@users.noreply.github.com>
2022-09-03 01:02:55 -0400

    feat: add `vane_proxy.start_server*` permission

--- vane-velocity/src/main/java/org/oddlama/velocity/compat/event/VelocityCompatPendingConnection.java
+++ vane-velocity/src/main/java/org/oddlama/velocity/compat/event/VelocityCompatPendingConnection.java
@@ -4,23 +4,28 @@ import com.velocitypowered.api.proxy.InboundConnection;
 import com.velocitypowered.api.proxy.Player;
 import org.jetbrains.annotations.Nullable;
 import org.oddlama.vane.proxycore.ProxyPendingConnection;
+import org.oddlama.vane.proxycore.ProxyServer;
 
 import java.net.SocketAddress;
+import java.util.Arrays;
 import java.util.UUID;
 
 public class VelocityCompatPendingConnection implements ProxyPendingConnection {
 
 	final InboundConnection connection;
 	final String username;
+	final UUID uuid;
 
 	public VelocityCompatPendingConnection(InboundConnection connection, String username) {
 		this.connection = connection;
 		this.username = username;
+		this.uuid = null;
 	}
 
 	public VelocityCompatPendingConnection(Player player) {
 		this.connection = player;
 		this.username = player.getUsername();
+		this.uuid = player.getUniqueId();
 	}
 
 	@Override
@@ -30,7 +35,7 @@ public class VelocityCompatPendingConnection implements ProxyPendingConnection {
 
 	@Override
 	public @Nullable UUID get_unique_id() {
-		return null;
+		return uuid;
 	}
 
 	@Override
@@ -41,6 +46,13 @@ public class VelocityCompatPendingConnection implements ProxyPendingConnection {
 	@Override
 	public SocketAddress get_socket_address() {
 		return connection.getVirtualHost().get();
+	}
+
+	@Override
+	public boolean has_permission(ProxyServer server, String... permission) {
+		// Safe cast, we don't use this outside of the case where this is constructed
+		// with a Player
+		return Arrays.stream(permission).anyMatch(perm -> ((Player)connection).hasPermission(perm));
 	}
 
 }
