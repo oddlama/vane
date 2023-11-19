7d3edb73182ace03546ab972bb078c6fd6e718b7
oddlama <oddlama@oddlama.org>
2023-11-19 21:13:24 +0100

    fix(portals): protect against rare player state corruption in papermc (can't use nether portals -> also affects our portals)

--- vane-portals/src/main/java/org/oddlama/vane/portals/PortalTeleporter.java
+++ vane-portals/src/main/java/org/oddlama/vane/portals/PortalTeleporter.java
@@ -181,7 +181,7 @@ public class PortalTeleporter extends Listener<Portals> {
 				entities_portalling.put(entity_id, event.getFrom().clone());
 			} else if (!get_module().portal_area_materials.contains(block.getType())) {
 				// At least 2 blocks away and outside of portal area → finish portalling.
-				if (event.getFrom().distance(loc) > 2.0) {
+				if (loc.getWorld() == event.getFrom().getWorld() && event.getFrom().distance(loc) > 2.0) {
 					entities_portalling.remove(entity_id);
 				}
 			}
